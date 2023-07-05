11544ae38c9194bb1a3577c21d380100fd4b14d9
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-07-05 13:32:33 +0300

    update core.py

---
+++ teagram/inline/core.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from .. import database
+
+_db = database.load_db()
+
+client = Client(
+    api_id = _db['api_id'],
+    api_hash = _db['api_hash'],
+    bot_token = _db['token']
+)
