76d29a2d319061da95b129666de1bc09cb805c39
HotDrify <105966726+HotDrify@users.noreply.github.com>
2023-07-05 20:48:17 +0300

    Update __init__.py

--- teagram/__init__.py
+++ teagram/__init__.py
@@ -1,3 +1,4 @@
 __authors__ = ["HotDrify", "itzlayz", "MuRuLOSE", "y9mphbphh", "codwiz", "y9lavacat"]
 __license__ = "GNU Affero General Public License v3.0"
 __copyright__ = "Copyright (C) 2023 TeaGram"
+__version__ = "0.0.1 DEV"
