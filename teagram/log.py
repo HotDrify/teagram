e24eb1cc1f181fdba2f5d8c6a66e3a1ba294fa68
agark0vlad21 <vladiscas@gmail.com>
2023-07-06 01:33:57 +0300

    Небольшое обновление модуля логирования

--- teagram/log.py
+++ teagram/log.py
@@ -1,8 +1,8 @@
-import logging
+from logging import basicConfig, DEBUG
 
-logging.basicConfig(
-  filename = "logs/debug.log",
-  filemode = 'w',
-  level = logging.DEBUG,
-  format = "(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+basicConfig(
+    filename="logs/debug.log",
+    filemode='w',
+    level=DEBUG,
+    format="(asctime)s - [%(levelname)s] -  %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
 )
