deceb467b4172d65305382fd31677af288828484
layz <99089467+itzlayz@users.noreply.github.com>
2023-07-05 15:27:06 +0600

    added api and hash id

--- teagram/database.py
+++ teagram/database.py
@@ -1,14 +1,23 @@
+"""
+тут находится бд
+
+    """
+
 import json
 
-def save_db(token: str, prefix: str = '.', modules: dict = None):
+def save_db(api_id: int, hash_id: str, token: str, prefix: str = '.', modules: dict = None):
     """_summary_
 
     Args:
+        api_id (int): _description_
+        hash_id (str): _description_
         token (str): _description_
         prefix (str, optional): _description_. Defaults to '.'.
         modules (dict, optional): _description_. Defaults to None.
     """
     data = {
+        'api_id': api_id,
+        'hash_id': hash_id,
         'token': token,
         'prefix': prefix,
         'modules': modules
@@ -27,4 +36,3 @@ def load_db():
         data = json.loads(file.read())
 
     return data
-
