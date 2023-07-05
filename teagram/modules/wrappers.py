1bc057c093389ed715db0c846ddf19b385a8424e
agark0vlad21 <vladiscas@gmail.com>
2023-07-05 21:37:57 +0300

    Улучшен wrappers.py

--- teagram/modules/wrappers.py
+++ teagram/modules/wrappers.py
@@ -1,27 +1,36 @@
-import inspect
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+from functools import wraps as _wraps
+from inspect import iscoroutinefunction
 from types import ModuleType
 from typing import Callable
 
 
-def wrap_to_async_function(function: Callable) -> Callable:
+def wrap_function_to_async(function: Callable) -> Callable:
     """
-    Делает из обычной функции асинхронную функцию
+    Оборачивает синхронные функции в асинхронные
     """
-    assert (inspect.iscoroutinefunction(function)), "Функция уже асинхронная!"
+    assert (not iscoroutinefunction(function)), "Функция уже асинхронная!"
 
-    async def async_function(*args, **kwargs):
-        function()
-    return async_function
+    pool = ThreadPoolExecutor()
+
+    @_wraps(function)
+    def wrapped(*args, **kwargs):
+
+        future = pool.submit(function, *args, **kwargs)
+        return asyncio.wrap_future(future)
+
+    return wrapped
 
 
 class WrapModuleToAsync:
     """
-    Делает то же самое что и wrap_to_async_function, но оборачивает уже целые модули
+    # Делает то же самое что и wrap_to_async_function, но оборачивает уже целые модули
     """
 
     def __init__(self, mod: ModuleType):
         for attr in dir(mod):
             item = getattr(mod, attr)
-            if callable(item) and not inspect.iscoroutinefunction(item):
-                wrapped = wrap_to_async_function(item)
+            if callable(item) and not iscoroutinefunction(item):
+                wrapped = wrap_function_to_async(item)
                 setattr(self, attr, wrapped)
