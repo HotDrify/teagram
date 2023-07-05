3ff0041b0ecf509531ad9b58b083cb217f282793
layz <99089467+itzlayz@users.noreply.github.com>
2023-07-05 13:30:35 +0600

    Update database.py

--- teagram/database.py
+++ teagram/database.py
@@ -1,4 +1,17 @@
-"""
-тут находится бд
+import json
 
-    """
+def save_db(token: str, prefix: str = '.', modules: dict = None):
+    data = {
+        'token': token,
+        'prefix': prefix,
+        'modules': modules
+    }
+
+    with open('config.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+def load_db():
+    with open('config.json', 'r') as file:
+        data = json.loads(file.read())
+
+    return data
