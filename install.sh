28da8420dc31a3af160c923a5e3f434ec8d8b22c
MuRuLOSE <134152147+MuRuLOSE@users.noreply.github.com>
2023-07-06 16:37:23 +0300

    сделал норм установщик

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-apt update && apt upgrade -y && apt install -y openssl git python3 python3-pip && git clone https://github.com/HotDrify/teagram && cd teagram && pip3 install -r requirements.txt && pip3 install -r requirements-speedup.txt && python3 -m teagram
+apt update
+apt upgrade -y
+apt install -y openssl git python3 python3-pip 
+git clone https://github.com/HotDrify/teagram 
+cd teagram 
+pip3 install -r requirements.txt 
+pip3 install -r requirements-speedup.txt 
+python3 -m teagram
