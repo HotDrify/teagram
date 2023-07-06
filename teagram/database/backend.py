bc8e07c436ca980f91903039e002e6cac700ac86
CodWiz <sergeimatrakhib@yandex.ru>
2023-07-06 12:43:52 +0300

    Add files via upload
    вот ваша бд

---
+++ teagram/database/backend.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from pyrogram import Client, types
+from typing import Union
+
+
+class CloudDatabase:
+    """Чат в Telegram с данными для базы данных"""
+
+    def __init__(self, app: Client, me: types.User):
+        self._app = app
+        self._me = me
+        self.data_chat = None
+
+        asyncio.get_event_loop().create_task(
+            self.find_data_chat())
+
+    async def find_data_chat(self):
+        """Информация о чате с данными"""
+        if not self.data_chat:
+            chat = [
+                dialog.chat async for dialog in self._app.iter_dialogs()
+                if dialog.chat.title == f"material-{self._me.id}-data"
+                and dialog.chat.type == "supergroup"
+            ]
+
+            if not chat:
+                self.data_chat = await self._app.create_supergroup(f"material-{self._me.id}-data")
+            else:
+                self.data_chat = chat[0]
+
+        return self.data_chat
+
+    async def save_data(self, message: Union[types.Message, str]):
+        """Сохранить данные в чат"""
+        return (
+            await self._app.send_message(
+                self.data_chat.id, message
+            )
+            if isinstance(message, str)
+            else await message.copy(self.data_chat.id)
+        )
+
+    async def get_data(self, message_id: int):
+        """Найти данные по айди сообщения"""
+        return await self._app.get_messages(
+            self.data_chat.id, message_id
+        )
---
+++ teagram/database/frontend.py
@@ -0,0 +1,50 @@
+from lightdb import LightDB
+from pyrogram import Client, types
+
+from typing import Union, KT, VT
+from . import CloudDatabase
+
+
+class Database(LightDB):
+    """Локальная база данных в файле"""
+
+    def __init__(self, location: str):
+        super().__init__(location)
+        self.cloud = None
+
+    def init_cloud(self, app: Client, me: types.User):
+        """Инициализация облачной базы данных"""
+        self.cloud = CloudDatabase(app, me)
+
+    def __repr__(self):
+        return object.__repr__(self)
+
+    def set(self, name: str, key: KT, value: VT):
+        self.setdefault(name, {})[key] = value
+        return self.save()
+
+    def get(self, name: str, key: KT, default: VT = None):
+        try:
+            return self[name][key]
+        except KeyError:
+            return default
+
+    def pop(self, name: str, key: KT = None, default: VT = None):
+        if not key:
+            value = self.pop(name, default)
+        else:
+            try:
+                value = self[name].pop(key, default)
+            except KeyError:
+                value = default
+
+        self.save()
+        return value
+
+    async def save_data(self, message: Union[types.Message, str]):
+        """Сохранить данные в чат"""
+        return await self.cloud.save_data(message)
+
+    async def get_data(self, message_id: int):
+        """Найти данные по айди сообщения"""
+        return await self.cloud.get_data(message_id)
