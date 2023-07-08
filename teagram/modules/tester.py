830634774ec68af24235b8c62113603d79e63c0e
CodWiz <sergeimatrakhib@yandex.ru>
2023-07-08 12:14:19 +0300

    Update tester.py
    Изи минус восемь

--- teagram/modules/tester.py
+++ teagram/modules/tester.py
@@ -10,7 +10,7 @@ from .. import loader, logger, utils
 @loader.module(name="Tester", author="teagram")
 class TesterMod(loader.Module):
     """Тест чего-то"""
-    async def logscmd(self, app: Client, message: types.Message, args: str):
+    async def logs_cmd(self, app: Client, message: types.Message, args: str):
         """Отправляет логи. Использование: logs <уровень>"""
         lvl = 40  # ERROR
 
