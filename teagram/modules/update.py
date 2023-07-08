33cfa4ce448851cf05e9c8f92840e0b2af8aaa07
layz <99089467+itzlayz@users.noreply.github.com>
2023-07-08 23:22:07 +0600

    added git update

---
+++ teagram/modules/update.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import atexit
+import logging
+
+from pyrogram import Client, types
+from subprocess import check_output
+from .. import loader, utils
+
+
+@loader.module(name="updater")
+class UpdateMod(loader.Module):
+    """🍵 Обновление с гита teagram"""
+
+    async def update_cmd(self, app: Client, message: types.Message):
+        try:
+            output = check_output('git pull', shell=True).decode()
+            
+            if 'Already up to date.' in output:
+                return await utils.answer(message, 'У вас установлена последняя версия')
+            
+            def restart() -> None:
+                os.execl(sys.executable, sys.executable, "-m", "teagram")
+
+            atexit.register(restart)
+            self.db.set(
+                "teagram.loader", "restart", {
+                    "msg": f"{message.chat.id}:{message.id}",
+                    "type": "restart"
+                }
+            )
+
+            await utils.answer(message, "🔁 Перезагрузка...")
+
+            logging.info("Перезагрузка...")
+            return sys.exit(0)
+        except Exception as error:
+            await utils.answer(message, f'Произошла ошибка: {error}')
