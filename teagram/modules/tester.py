665ff470e4004bb7b3ddb6b51a437cf38bac5037
layz <99089467+itzlayz@users.noreply.github.com>
2023-07-09 16:09:16 +0600

    ping -> tester

--- teagram/modules/tester.py
+++ teagram/modules/tester.py
@@ -122,3 +122,16 @@ class TesterMod(loader.Module):
                 for alias, command in aliases.items()
             )
         )
+
+    async def ping_cmd(self, app: Client, message: types.Message, args: str):
+        """🍵 команда для просмотра пинга."""
+        start = time.perf_counter_ns()
+        await utils.answer(message, "☕")
+        ping = round((time.perf_counter_ns() - start) / 10**6, 3)
+        await utils.answer(
+            message,
+            f"""
+🍵 `Teagram | UserBot`
+🏓 **Понг!**: `{ping}ms`
+            """
+        )
