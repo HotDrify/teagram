ea6978c3b2641fa7a618900830d5d1e2d78b05e7
layz <99089467+itzlayz@users.noreply.github.com>
2023-08-12 23:41:01 +0600

    Можно отключать проверку модулей

--- teagram/modules/moduleGuard.py
+++ teagram/modules/moduleGuard.py
@@ -6,85 +6,109 @@ import os
 @loader.module(name="ModuleGuard", author='teagram')
 class ModuleGuardMod(loader.Module):
     """moduleGuard оповестит вас о вредоносном модуле."""
-    async def on_load(self, message: types.Message):
-        names = {
-            "info": [
-                {"id": "other", "name": "other"},
-                {"id": "other", "name": "other"}
-            ],
-            "warns": [
-                {"id": "eval", "name": "Eval"},
-                {"id": "exec", "name": "Exec"}
-            ],
-            "criticals": [
-                {"id": "session", "name": "plugin can get session"},
-                {"id": "config.ini", "name": "plugin can get auth data (config.ini)"}
-            ]
-        }
+    async def on_load(self, app: types.Message):
+        if self.db.get('Guard', 'send_on_load'):
+            names = {
+                "info": [
+                    {"id": "other", "name": "other"},
+                    {"id": "other", "name": "other"}
+                ],
+                "warns": [
+                    {"id": "eval", "name": "Eval"},
+                    {"id": "exec", "name": "Exec"}
+                ],
+                "criticals": [
+                    {"id": "session", "name": "plugin can get session"},
+                    {"id": "config.ini", "name": "plugin can get auth data (config.ini)"}
+                ]
+            }
 
-        basic_plugins = ['eval.py', '_example.py', 'help.py', 
-                         'info.py', 'loader.py', 'moduleGuard.py',
-                         'terminal.py', 'tester.py', 'translator.py',
-                         'updater.py', 'backup.py']
-                         
-        critical = []
-        warns = []
-        info = []
-        file_list = os.listdir("teagram/modules/")
+            basic_plugins = ['eval.py', '_example.py', 'help.py', 
+                            'info.py', 'loader.py', 'moduleGuard.py',
+                            'terminal.py', 'tester.py', 'translator.py',
+                            'updater.py', 'backup.py']
+                            
+            critical = []
+            warns = []
+            info = []
+            file_list = os.listdir("teagram/modules/")
 
-        for file_name in file_list:
-            if file_name in basic_plugins:
-                continue
-            else:
-                file_path = os.path.join("teagram/modules/", file_name)
-                if os.path.isfile(file_path):
-                    with open(file_path, "r") as file:
-                        try:
-                            content = file.read()
-                        except UnicodeDecodeError:
-                            continue
+            for file_name in file_list:
+                if file_name in basic_plugins:
+                    continue
+                else:
+                    file_path = os.path.join("teagram/modules/", file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, "r") as file:
+                            try:
+                                content = file.read()
+                            except UnicodeDecodeError:
+                                continue
 
+                        for word in names["warns"]:
+                            if word['id'] in content:
+                                warns.append(word["name"])
 
-                    for word in names["warns"]:
-                        if word['id'] in content:
-                            warns.append(word["name"])
+                        for word in names["criticals"]:
+                            if word['id'] in content:
+                                critical.append(word["name"])
 
-                    for word in names["criticals"]:
-                        if word['id'] in content:
-                            critical.append(word["name"])
+                        # for word in names["info"]:
+                        #     if word['id'] in content:
+                        #         info.append(word["name"])
 
-                    # for word in names["info"]:
-                    #     if word['id'] in content:
-                    #         info.append(word["name"])
-
-        message_text = """
+            message_text = """
 <b>ModuleGuard</b>
-"""
-        basic_text = """
+    """
+            basic_text = """
 <b>ModuleGuard</b>
-"""
-        for file_name in file_list:
-            if not file_name.endswith('.py'):
-                continue
-            if file_name in basic_plugins:
-                continue
-            else:
-                info_text = ', '.join(info)
-                warns_text = ', '.join(warns)
-                critical_text = ', '.join(critical)
-                message_text += f"{file_name}:\n"
+    """
+            for file_name in file_list:
+                if not file_name.endswith('.py'):
+                    continue
+                if file_name in basic_plugins:
+                    continue
+                else:
+                    info_text = ', '.join(info)
+                    warns_text = ', '.join(warns)
+                    critical_text = ', '.join(critical)
+                    message_text += f"{file_name}:\n"
 
-                if info_text:
-                    message_text += f"❔ Info ➜ {info_text}\n"
-                if warns_text:
-                    message_text += f"❗ Warns ➜ {warns_text}\n"
-                if critical_text:
-                    message_text += f"❌ Criticals ➜ {critical_text}\n"
+                    if info_text:
+                        message_text += f"❔ Info ➜ {info_text}\n"
+                    if warns_text:
+                        message_text += f"❗ Warns ➜ {warns_text}\n"
+                    if critical_text:
+                        message_text += f"❌ Criticals ➜ {critical_text}\n"
 
-                if not info and not warns and not critical:
-                    message_text += 'Безопасный плагин ✔\n'
+                    if not info and not warns and not critical:
+                        message_text += 'Безопасный плагин ✔\n'
+            
+            if message_text == basic_text:
+                message_text += 'Подозрительных плагинов не найдено'
+
+            await app.send_message("me", message_text)
+
+    async def guard_cmd(self, app: Client, message: types.Message, args: str):
+        if not (turn := args.lower()) or turn.strip() not in ['on', 'off']:
+            return await utils.answer(
+                message,
+                '❌ Вы не указали аргументы (on/off)'
+            )
+
+        turn = turn.split()[0]
         
-        if message_text == basic_text:
-            message_text += 'Подозрительных плагинов не найдено'
+        if turn == 'on':
+            self.db.set('Guard', 'send_on_load', True)
 
-        await message.send_message("me", message_text)
+            await utils.answer(
+                message,
+                '✔ При загрузке будет проверка модулей!'
+            )
+        else:
+            self.db.set('Guard', 'send_on_load', False)
+
+            await utils.answer(
+                message,
+                '✔ При загрузке не будет проверки модулей!'
+            )
