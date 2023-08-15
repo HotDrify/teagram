386b10e0b830e9405b6ca380d6ac2b5421fca307
layz <99089467+itzlayz@users.noreply.github.com>
2023-08-15 20:32:13 +0600

    Вернул старый евал на время

--- teagram/modules/eval.py
+++ teagram/modules/eval.py
@@ -1,7 +1,17 @@
-import subprocess
+import ast
 
 from .. import loader, utils
 from pyrogram import Client, types
+
+def insert_returns(body):
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+        if isinstance(body[-1], ast.If):
+            insert_returns(body[-1].body)
+            insert_returns(body[-1].orelse)
+        if isinstance(body[-1], ast.With):
+            insert_returns(body[-1].body)
 
 async def execute_python_code(code, env={}):
     # sourcery skip: inline-immediately-returned-variable
@@ -9,16 +19,12 @@ async def execute_python_code(code, env={}):
         fn_name = "_eval_expr"
         cmd = "\n".join(f" {i}" for i in code.splitlines())
         body = f"async def {fn_name}():\n{cmd}"
-        
-        p = subprocess.Popen(['python', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        stdout, stderr = p.communicate(body)
-        
-        if stderr:
-            return stderr
-        
-        env = {'import': __import__, **env}
-        exec(compile(stdout, filename="<ast>", mode="exec"), env)
-        result = await eval(f"{fn_name}()", env)
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+        insert_returns(body)
+        env = {'__import__': __import__, **env}
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+        result = (await eval(f"{fn_name}()", env))
         
         return result
     except Exception as error:
