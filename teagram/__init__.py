3413cc8bcae17a20ea5fb2244d906c080c7ad2fa
layz <99089467+itzlayz@users.noreply.github.com>
2023-09-14 23:58:37 +0600

    0.0.7
    веб есть значит новая версия

--- teagram/__init__.py
+++ teagram/__init__.py
@@ -1,4 +1,4 @@
 __authors__ = ["HotDrify", "itzlayz", "codwiz", "y9lavacat"]
 __license__ = "GNU Affero General Public License v3.0"
 __copyright__ = "Copyright (C) 2023 Teagram"
-__version__ = "0.0.6 DEV"
+__version__ = "0.0.7 DEV"
