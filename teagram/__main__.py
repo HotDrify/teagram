40ce0a9ef8fe739093d4d3c43239e5315fe0d5c2
layz <99089467+itzlayz@users.noreply.github.com>
2023-09-17 20:22:56 +0600

    Обнова
    удалены лишние файлы (оптимизацию никто не юзал)
    фикс терминала, теперь выводит ошибку

--- teagram/__main__.py
+++ teagram/__main__.py
@@ -1,48 +1,12 @@
 import sys
 
-from . import logger, main
+from . import main
 
 if sys.version_info < (3, 9, 0):
     print("Требуется Python 3.9 или выше")
     sys.exit(1)
 
-
-from argparse import ArgumentParser
 import asyncio
-
-
-def parse_arguments():
-    parser = ArgumentParser(
-        prog="TeaGram", description="Юзербот",
-        add_help=True
-    )
-    parser.add_argument("--list-optimizations",
-                        action="store_true",
-                        help="Вывести доступные оптимизации",
-                        default=False
-                        )
-    parser.add_argument("-n",
-                        "--no-optimizations",
-                        action="store_false",
-                        default=True,
-                        help="Выключить использование оптимизаций"
-                        )
-    return parser.parse_args()
-
-
-args = parse_arguments()
-
-if args.list_optimizations:
-    from . import optimizations
-    optimizations_list = optimizations.apply_optimizations()
-    print(f"Доступно {len(optimizations_list)} оптимизаций:")
-    for o in optimizations_list:
-        print(" - " + o)
-    exit()
-
-if args.no_optimizations:
-    from . import optimizations
-    optimizations.apply_optimizations()
 
 if __name__ == "__main__":
 
--- teagram/modules/terminal.py
+++ teagram/modules/terminal.py
@@ -1,21 +1,26 @@
-from subprocess import check_output
+import asyncio
+
 from pyrogram import Client, types
-
 from .. import loader, utils
-from ..wrappers import wrap_function_to_async
 
-@wrap_function_to_async
-def bash_exec(args: str):
-    try:
-        output = check_output(args.strip(), shell=True)
-        output = output.decode()
-
-        return output
-    except UnicodeDecodeError:
-        return check_output(args.strip(), shell=True)
-    except Exception as error:
-        return error
-
+async def bash_exec(command):
+    a = await asyncio.create_subprocess_shell(
+        command.strip(), 
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    
+    if not (out := await a.stdout.read(-1)):
+        try:
+            return (await a.stderr.read(-1)).decode()
+        except UnicodeDecodeError:
+            return f'Unicode decode error: {(await a.stderr.read(-1))}'
+    else:
+        try:
+            return out.decode()
+        except UnicodeDecodeError:
+            return f'Unicode decode error: {out}'
 
 @loader.module(name="Terminal", author='teagram')
 class TerminalMod(loader.Module):
