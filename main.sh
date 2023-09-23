8946f6a5907f833bc905461ee9fa524cd80e8ae2
tigca <84330292+tigca@users.noreply.github.com>
2023-09-24 00:08:14 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,6 +20,24 @@ LOG_FILE="install.log"
 echo "[INFO] Starting installation" > "$LOG_FILE"
 echo "[DEBUG] SUDOCMD: $SUDOCMD" >> "$LOG_FILE"
 
+if [[ "$teagram" == "reset" ]]; then
+    echo "[INFO] Resetting teagram..." >> "$LOG_FILE"
+    eval "$SUDOCMD apt purge -y python3"
+fi
+
+if command -v python3.11 &>/dev/null; then
+    PYTHON="python3.11"
+elif command -v python3.10 &>/dev/null; then
+    PYTHON="python3.10"
+elif command -v python3.9 &>/dev/null; then
+    PYTHON="python3.9"
+else
+    echo "[ERROR] Python 3.11, 3.10, or 3.9 not found. See logs for more information."
+    exit 1
+fi
+
+echo "[INFO] Using Python: $PYTHON" >> "$LOG_FILE"
+
 if [[ "$OSTYPE" == *linux-gnu* ]]; then
     echo "[INFO] Found OS type: GNU/Linux ($OSTYPE)" >> "$LOG_FILE"
     PKGINSTALL="apt install -y"
@@ -40,6 +58,10 @@ elif [[ -f /etc/gentoo-release ]]; then
     echo "[INFO] Found OS type: Gentoo" >> "$LOG_FILE"
     PKGINSTALL="emerge -u"
     UPD="emerge --sync && emerge -uDN @world"
+elif [[ -f /etc/nixos/configuration.nix ]]; then
+    echo "[INFO] Found OS type: NixOS" >> "$LOG_FILE"
+    PKGINSTALL="nix-env -i"
+    UPD="nix-channel --update && nix-env -u '*'"
 else
     echo "[ERROR] OS type not found: $OSTYPE" >> "$LOG_FILE"
     echo "[ERROR] OS not found. See logs for more information."
@@ -58,4 +80,4 @@ pip3 install -r requirements.txt
 echo "[INFO] First start teagram..." >> "$LOG_FILE"
 echo "[INFO] First start..."
 clear
-python3 -m teagram
+$PYTHON -m teagram
