6b442112f0f9d33c4b67f85d417a8edcf5e36551
layz <99089467+itzlayz@users.noreply.github.com>
2023-10-08 13:54:20 +0600

    Версия

--- teagram/__init__.py
+++ teagram/__init__.py
@@ -1,6 +1,6 @@
 __authors__ = ["HotDrify", "itzlayz", "codwiz", "y9lavacat"]
 __license__ = "GNU Affero General Public License v3.0"
 __copyright__ = "Copyright (C) 2023 Teagram"
-__version__ = "0.0.9"
+__version__ = "1.0.0"
 
 init_time = __import__('time').time()
