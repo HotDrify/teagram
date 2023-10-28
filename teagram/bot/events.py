dc05d4000566a2be60d4ac2522299e4dc1e82db1
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-10-28 13:19:00 +0300

    ура теперь вместо префикс префикс

--- teagram/bot/events.py
+++ teagram/bot/events.py
@@ -35,7 +35,7 @@ class Events(Item):
                 photo='https://github.com/itzlayz/teagram-tl/blob/main/assets/bot_avatar.png?raw=true',
                 caption='☕ Добро пожаловать! Это инлайн бот <b>Teagram</b>\n'
                 '✒ Предлагаем вам настроить конфиг\n'
-                '✒ Используйте инлайн команду <b>префикс</b><code>config</code>',
+                '✒ Используйте инлайн команду <code>{}config</code>'.format(self.manager._db.get('teagram.loader', 'prefixes', ['.']),
                 parse_mode='html'
             )
 
