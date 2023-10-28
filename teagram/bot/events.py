4e0940db6aeeb1cc7a5b46330fd5b4e16af98e81
Macsim <134152147+MuRuLOSE@users.noreply.github.com>
2023-10-28 13:22:54 +0300

    Update events.py

--- teagram/bot/events.py
+++ teagram/bot/events.py
@@ -35,7 +35,7 @@ class Events(Item):
                 photo='https://github.com/itzlayz/teagram-tl/blob/main/assets/bot_avatar.png?raw=true',
                 caption='☕ Добро пожаловать! Это инлайн бот <b>Teagram</b>\n'
                 '✒ Предлагаем вам настроить конфиг\n'
-                f'✒ Используйте инлайн команду <code>{self.manager._db.get('teagram.loader', 'prefixes', ['.'])[0]}config</code>',
+                f'✒ Используйте инлайн команду <code>{self.manager._db.get('teagram.loader', 'prefixes', ['.'])}config</code>',
                 parse_mode='html'
             )
 
