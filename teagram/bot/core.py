9954d87375f9a27683395c8aaaf3c957a4e61359
Sourcery AI <>
2023-11-02 09:47:48 +0000

    'Refactored by Sourcery'

--- teagram/bot/core.py
+++ teagram/bot/core.py
@@ -69,9 +69,9 @@ class BotManager(Events, TokenManager):
 
 
             self._token = await self._create_bot()
-            if not self._token:
-                logging.error(error_text)
-                sys.exit(1)
+        if not self._token:
+            logging.error(error_text)
+            sys.exit(1)
 
         try:
             self.bot = Bot(self._token, parse_mode="html")
