0c8ccae0cc94ade15bbe1cb56469f112d50c4d65
layz <broitzlayz@gmail.com>
2024-02-03 23:20:38 +0600

    teagram-pyro isn't updating

--- teagram/__main__.py
+++ teagram/__main__.py
@@ -6,11 +6,12 @@ if sys.version_info < (3, 9, 0):
     print("Требуется Python 3.9 или выше")
     sys.exit(1)
 
+prompt = input("Teagram-pyro isn't updating, so all bugs is your problems, continue? y\n: ")
+if "y" not in prompt.lower():
+    sys.exit(1)
+
 import asyncio
-
 if __name__ == "__main__":
-
-
     # import logging                          # РАЗКОММЕНТИРУЙТЕ ЭТО ЕСЛИ У ВАС БЕСКОНЕЧНАЯ ЗАГРУЗКА, И ОТПРАВЬТЕ ЛОГИ В САППОРТ ЧАТ https://t.me/UBteagram/974
     # logging.basicConfig(level=logging.INFO) # UNCOMMENT THIS IF YOU HAVE INFINITY LOADING, AND SEND LOGS TO SUPPORT CHAT https://t.me/UBteagram/974
     
